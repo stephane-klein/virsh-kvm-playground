641ccb323561c3f569865b5516c307e8c9629441
Stéphane Klein <contact@stephane-klein.info>
2023-02-22 16:16:52 +0100

    Improve destroy script

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -3,9 +3,12 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-# virsh shutdown fedora37
-
 LANG=C
+
+state=$(virsh list --all | grep " fedora37 " | awk '{ print $3}')
+if [ "$state" == "running" ]; then
+    virsh destroy fedora37
+fi
 
 state=$(virsh list --all | grep " fedora37 " | awk '{ print $3}')
 if [ "$state" == "shut" ]; then
