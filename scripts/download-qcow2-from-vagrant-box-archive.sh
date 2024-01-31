d71d22d5341293d403656568b9e5384a853d282c
Stéphane Klein <contact@stephane-klein.info>
2024-01-31 21:43:02 +0100

    WIP

--- scripts/download-qcow2-from-vagrant-box-archive.sh
+++ scripts/download-qcow2-from-vagrant-box-archive.sh
@@ -5,10 +5,10 @@ cd "$(dirname "$0")/../"
 
 mkdir -p qcow2-images
 curl -L --progress-bar \
-    -o qcow2-images/fedora37.box \
-    https://app.vagrantup.com/generic/boxes/fedora37/versions/4.2.14/providers/libvirt.box
+    -o qcow2-images/fedora39.box \
+    https://app.vagrantup.com/generic/boxes/fedora39/versions/4.3.12/providers/qemu/arm64/vagrant.box
 
-tar xvf qcow2-images/fedora37.box -C qcow2-images box.img
-mv qcow2-images/box.img qcow2-images/fedora37.qcow2
+tar xvf qcow2-images/fedora39.box -C qcow2-images box_0.img
+mv qcow2-images/box_0.img qcow2-images/fedora39.qcow2
 
-rm qcow2-images/fedora37.box
+rm qcow2-images/fedora39.box
